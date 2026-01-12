3d053dd4ded756d5cde7f8635139ef979085e568
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-01-12 18:00:06 +0000

    trainer: Add from_superbatch util for use when resuming training

--- tools/trainer/src/bullet_extensions.rs
+++ tools/trainer/src/bullet_extensions.rs
@@ -2,6 +2,9 @@ use bullet_lib::TrainingSteps;
 
 pub trait TrainingStepsImpl {
     fn default(n_superbatches: usize) -> Self;
+
+    #[allow(unused, reason = "Only used when resuming training")]
+    fn from_superbatch(n_superbatches: usize, start_superbatch: usize) -> Self;
 }
 
 impl TrainingStepsImpl for TrainingSteps {
@@ -12,5 +15,11 @@ impl TrainingStepsImpl for TrainingSteps {
             start_superbatch: 1,
             end_superbatch: n_superbatches,
         }
+    }
+
+    fn from_superbatch(n_superbatches: usize, start_superbatch: usize) -> Self {
+        let mut steps = Self::default(n_superbatches);
+        steps.start_superbatch = start_superbatch;
+        steps
     }
 }

f4a824d651e4c9c1800e76a73d10639ff8a5ba4f
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-02-10 17:54:33 +0000

    Log a backtrace if the engine panics
    This helps massively with diagnosing crashes. The backtrace includes
    lots of non-engine info which we remove (with .contains() since we can't
    access the frames directly yet).

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,15 +1,23 @@
-use std::{panic::PanicHookInfo, process::ExitCode};
+use std::{
+    backtrace::{Backtrace, BacktraceStatus},
+    fmt::Write,
+    panic::PanicHookInfo,
+    process::ExitCode,
+};
 
 use engine::engine::util::log;
 
-fn get_panic_message(info: &PanicHookInfo<'_>) -> String {
-    if let Some(s) = info.payload().downcast_ref::<&str>() {
-        format!("panic occurred: {s:?} {info:?}")
-    } else if let Some(s) = info.payload().downcast_ref::<String>() {
-        format!("panic occurred: {s:?} {info:?}")
-    } else {
-        format!("{info:?}")
-    }
+fn main() -> ExitCode {
+    std::panic::set_hook(Box::new(|info| {
+        let backtrace = Backtrace::capture();
+        let panic_message = get_panic_message(info, &backtrace);
+
+        println!("{panic_message}");
+        log::crashlog(panic_message);
+    }));
+
+    engine::init();
+    run()
 }
 
 fn run() -> ExitCode {
@@ -39,14 +47,52 @@ fn run() -> ExitCode {
     }
 }
 
-fn main() -> ExitCode {
-    std::panic::set_hook(Box::new(|info| {
-        let panic_message = get_panic_message(info);
+fn get_panic_message(info: &PanicHookInfo<'_>, backtrace: &Backtrace) -> String {
+    let mut message = if let Some(s) = info.payload().downcast_ref::<&str>() {
+        format!("panic occurred: {s:?} {info:?}")
+    } else if let Some(s) = info.payload().downcast_ref::<String>() {
+        format!("panic occurred: {s:?} {info:?}")
+    } else {
+        format!("{info:?}")
+    };
 
-        println!("{panic_message}");
-        log::crashlog(panic_message);
-    }));
+    if backtrace.status() == BacktraceStatus::Captured {
+        let _ = write!(message, "\n{}", format_backtrace(backtrace));
+    }
 
-    engine::init();
-    run()
+    message
+}
+
+fn format_backtrace(backtrace: &Backtrace) -> String {
+    if backtrace.status() != BacktraceStatus::Captured {
+        return "<no backtrace>".to_string();
+    }
+
+    let lines = format!("{backtrace:#?}");
+    let lines = lines.lines().collect::<Vec<_>>();
+    let mut lines = lines.iter();
+
+    // Skip the first line: Backtrace [
+    lines.next();
+
+    let is_not_our_line = |s: &str| {
+        s.contains("std::")
+            || s.contains("core::")
+            || s.contains("<alloc::")
+            || s.contains("__rustc")
+            || s.contains("__rust_try")
+            || s.contains("__pthread")
+            // Always exclude the frame from the panic handling code
+            || s.contains("engine::main::{{closure}}")
+    };
+
+    let mut message = String::new();
+
+    for line in lines {
+        if !is_not_our_line(line) {
+            let _ = write!(message, "\n{line}");
+        }
+    }
+
+    message
 }

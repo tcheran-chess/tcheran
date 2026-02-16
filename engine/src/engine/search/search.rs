tcheran-chess/tcheran

commit c5eed34bb862d62f7f14459b47f18162fb0baacd

Move root tablebase probing code into its own function
This de-clutters the start of the main search method.

Non-functional without tablebases:

Elo   | -0.00 +- 0.00 (95%)
SPRT  | N=20000 Threads=1 Hash=16MB
LLR   | 0.25 (-2.25, 2.89) [-5.00, 0.00]
Games | N: 1214 W: 358 L: 358 D: 498
Penta | [0, 0, 607, 0, 0]

Non-functional with tablebases and without adjudication:

Elo   | -0.00 +- 0.00 (95%)
SPRT  | N=20000 Threads=1 Hash=16MB
LLR   | -0.16 (-2.25, 2.89) [0.00, 5.00]
Games | N: 800 W: 219 L: 219 D: 362
Penta | [0, 0, 400, 0, 0]

engine/src/engine/search/search.rs
@@ -239,7 +239,7 @@ pub struct SearchResult {
 pub struct SearchInfo<'s> {
     pub game: &'s Game,
     pub eval: Eval,
-    pub pv: &'s PrincipalVariation,
+    pub pv: PrincipalVariation,
     pub stats: SearchStats,
 }
 
@@ -296,34 +296,15 @@ pub fn search(
     options: &EngineOptions,
     reporter: &impl Reporter,
 ) -> (Move, Eval) {
-    persistent_state.tt.new_generation();
-
-    let tablebase_result = persistent_state.tablebase.best_move(game);
-    if let Some(mv) = tablebase_result {
-        let start_time = Instant::now();
-        let (pv, eval) = get_tablebase_pv(game, &persistent_state.tablebase);
-        let elapsed = start_time.elapsed();
-
-        let depth = pv.len();
-
-        reporter.report_search_progress(SearchInfo {
-            game,
-            eval,
-            pv: &pv,
-            stats: SearchStats {
-                time: elapsed,
-                depth,
-                seldepth: depth,
-                nodes: u64::from(depth),
-                nodes_per_second: util::metrics::nodes_per_second(u64::from(depth), elapsed),
-                tbhits: 1,
-                hashfull: 0,
-            },
-        });
-
+    if let Some((mv, eval, info)) =
+        probe_tb_at_root(game, &persistent_state.tablebase, &time_control)
+    {
+        reporter.report_search_progress(info);
         reporter.best_move(game, mv);
         return (mv, eval);
     }
+
+    persistent_state.tt.new_generation();
 
     let threads_stop_control = StopControl::new();
     let global_node_count = AtomicU64::new(0);
@@ -416,7 +397,7 @@ pub fn search(
         reporter.report_search_progress(SearchInfo {
             game,
             eval: result.eval,
-            pv: &result.pv,
+            pv: result.pv.clone(),
             stats: SearchStats::from_ctx(&ctx),
         });
 
@@ -427,26 +408,46 @@ pub fn search(
     (result.best_move, result.eval)
 }
 
-fn get_tablebase_pv(game: &Game, tb: &Tablebase) -> (PrincipalVariation, Eval) {
-    let mut game = game.clone();
+fn probe_tb_at_root<'s>(
+    game: &'s Game,
+    tb: &Tablebase,
+    time_control: &TimeControl,
+) -> Option<(Move, Eval, SearchInfo<'s>)> {
+    if !tb.is_enabled {
+        return None;
+    }
+
+    if game.board.occupancy().count() > tb.n_men() {
+        return None;
+    }
+
+    let best_move = tb.best_move(game)?;
+
+    let start_time = match time_control {
+        TimeControl::Clocks { start_time, .. } | TimeControl::ExactTime { start_time, .. } => {
+            *start_time
+        }
+        _ => Instant::now(),
+    };
+
     let player = game.player;
+    let mut g = game.clone();
 
     let mut pv = PrincipalVariation::new();
 
     let tb_score = tb
-        .wdl(&game)
+        .wdl(game)
         .expect("In tablebase position, but unable to get tablebase score");
 
     let mut eval = None;
 
     for _ in 0..MAX_SEARCH_DEPTH {
         let tablebase_move = tb
-            .best_move(&game)
+            .best_move(game)
             .expect("In tablebase position, but unable to get tablebase move");
 
         pv.append(tablebase_move);
-
-        game.make_move(tablebase_move);
+        g.make_move(tablebase_move);
 
         // Check if this move terminated the game, and return an appropriate score
         let legal_moves = game.moves();
@@ -469,12 +470,30 @@ fn get_tablebase_pv(game: &Game, tb: &Tablebase) -> (PrincipalVariation, Eval) {
         }
     }
 
-    (
-        pv,
-        eval.unwrap_or_else(|| match tb_score {
-            Wdl::Win => Eval::mate_in(1),
-            Wdl::Draw => Eval::DRAW,
-            Wdl::Loss => Eval::mated_in(1),
-        }),
-    )
+    let elapsed = start_time.elapsed();
+    let depth = pv.len();
+    let eval = eval.unwrap_or_else(|| match tb_score {
+        Wdl::Win => Eval::mate_in(1),
+        Wdl::Draw => Eval::DRAW,
+        Wdl::Loss => Eval::mated_in(1),
+    });
+
+    Some((
+        best_move,
+        eval,
+        SearchInfo {
+            game,
+            pv: pv.clone(),
+            eval,
+            stats: SearchStats {
+                time: elapsed,
+                depth,
+                seldepth: depth,
+                nodes: u64::from(depth),
+                nodes_per_second: util::metrics::nodes_per_second(u64::from(depth), elapsed),
+                tbhits: 1,
+                hashfull: 0,
+            },
+        },
+    ))
 }

engine/src/engine/search/iterative_deepening.rs
@@ -47,7 +47,7 @@ pub fn search(
         reporter.report_search_progress(SearchInfo {
             game,
             eval,
-            pv: &pv,
+            pv: pv.clone(),
             stats: SearchStats::from_ctx(ctx),
         });
     }

engine/src/engine/tablebases/tablebases.rs
@@ -22,7 +22,7 @@ pub enum Wdl {
 
 #[derive(Clone)]
 pub struct Tablebase {
-    is_enabled: bool,
+    pub is_enabled: bool,
 }
 
 impl Tablebase {

commit ab864fd4aa5bb1cfe93c976474f10287d01b9e76

Fix a crash in root tablebase probing
This was introduced in c5eed34bb862d62f7f14459b47f18162fb0baacd, and
causes the engine to crash when it reaches any tablebase position.

This should have been caught immediately, but all of my OpenBench
runners have 5-man Syzygy tables and tablebase adjudication is enabled.
Therefore, the game was always adjudicated right before the engine would
have crashed.

The issue is simply that while probing the tablebase repeatedly to get a
PV, the original Game is used instead of the one where we have made the
move, so the same tablebase move is probed and then applied to a
position where it is illegal.

engine/src/engine/search/search.rs
@@ -443,21 +443,21 @@ fn probe_tb_at_root<'s>(
 
     for _ in 0..MAX_SEARCH_DEPTH {
         let tablebase_move = tb
-            .best_move(game)
+            .best_move(&g)
             .expect("In tablebase position, but unable to get tablebase move");
 
         pv.append(tablebase_move);
         g.make_move(tablebase_move);
 
         // Check if this move terminated the game, and return an appropriate score
-        let legal_moves = game.moves();
-        let king_in_check = game.in_check();
+        let legal_moves = g.moves();
+        let king_in_check = g.in_check();
 
         if legal_moves.is_empty() {
             eval = Some(if king_in_check {
                 let plies = pv.len();
 
-                if game.player == player {
+                if g.player == player {
                     Eval::mated_in(plies)
                 } else {
                     Eval::mate_in(plies)
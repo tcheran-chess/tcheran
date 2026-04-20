34e36ba9329cf9206cdaa9a2b9e4bb0bb7891aca
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-04-20 12:46:38 +0100

    Use our forked version of Bullet which supports Metal
    Thanks to Blightwidow for the initial changes to support Metal in bullet.

--- tools/trainer/src/main.rs
+++ tools/trainer/src/main.rs
@@ -1,16 +1,5 @@
-cfg_select! {
-    target_os = "macos" => {
-        mod trainer {
-            pub fn run() {
-                println!("Cannot run on macOS");
-            }
-        }
-    }
-    _ => {
-        mod bullet_extensions;
-        mod trainer;
-    }
-}
+mod bullet_extensions;
+mod trainer;
 
 fn main() {
     trainer::run();

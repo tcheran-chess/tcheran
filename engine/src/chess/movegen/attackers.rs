12925290c5ee8a34229e7101540e35ea1c03a864
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-05-13 14:24:11 +0100

    Remove unused generate_attackers_of

--- engine/src/chess/movegen/attackers.rs
+++ engine/src/chess/movegen/attackers.rs
@@ -2,27 +2,6 @@ use crate::chess::{
     bitboard::Bitboard, board::Board, movegen::tables, player::Player, square::Square,
 };
 
-pub fn generate_attackers_of(board: &Board, player: Player, square: Square) -> Bitboard {
-    let mut attackers = Bitboard::EMPTY;
-    let them = player.other();
-    let all_pieces = board.occupancy();
-
-    // Pawns: A square is attacked by pawns in the same positions as a pawn could capture if it was on
-    // that square
-    attackers |= tables::pawn_attacks(square, player) & board.pawns(them);
-
-    // Knights: A square is attacked by any squares a knight could reach if it were on that square
-    attackers |= tables::knight_attacks(square) & board.knights(them);
-
-    // Sliders: A square is attacked by any squares sliding pieces can reach
-    attackers |= tables::bishop_attacks(square, all_pieces) & board.diagonal_sliders(them);
-    attackers |= tables::rook_attacks(square, all_pieces) & board.orthogonal_sliders(them);
-
-    attackers |= tables::king_attacks(square) & board.king(them);
-
-    attackers
-}
-
 pub fn all_attackers_of(board: &Board, square: Square, occupied: Bitboard) -> Bitboard {
     use Player::*;
 
--- engine/src/chess/movegen/mod.rs
+++ engine/src/chess/movegen/mod.rs
@@ -2,7 +2,7 @@ pub mod attackers;
 mod moves;
 pub mod tables;
 
-pub use attackers::{all_attackers_of, generate_attackers_of};
+pub use attackers::all_attackers_of;
 pub use moves::{generate_legal_moves, generate_quiets, generate_tacticals};
 
 pub fn init() {

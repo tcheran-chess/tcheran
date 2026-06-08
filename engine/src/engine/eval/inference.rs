322f9497f8e7643d48161f501a944609330fd25f
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-06-08 12:15:10 +0100

    Rewrite sum_output_weights_scalar
    Using pointer arithmetic is going to make it easier to rewrite this for
    pairwise.

--- engine/src/engine/eval/inference.rs
+++ engine/src/engine/eval/inference.rs
@@ -110,17 +110,24 @@ fn sum_output_weights_simd(us: &Accumulator, them: &Accumulator, output_bucket: 
 #[allow(unused, reason = "May be unused when SIMD is available")]
 fn sum_output_weights_scalar(us: &Accumulator, them: &Accumulator, output_bucket: usize) -> i32 {
     let output_weights = &NETWORK.output_weights[output_bucket];
+    let us_output_weights = &output_weights[..HIDDEN_SIZE];
+    let them_output_weights = &output_weights[HIDDEN_SIZE..];
 
     let mut output = 0;
 
-    for (&us, &weight) in us.0.iter().zip(&output_weights[..HIDDEN_SIZE]) {
-        let us_clamped = us.clamp(0, QA);
-        output += i32::from(us_clamped * weight) * i32::from(us_clamped);
-    }
+    unsafe {
+        for i in 0..HIDDEN_SIZE {
+            let us: *const i16 = us.0.as_ptr().add(i).cast();
+            let us_clamped = (*us).clamp(0, QA);
+            let us_weight: *const i16 = us_output_weights.as_ptr().add(i).cast();
 
-    for (&them, &weight) in them.0.iter().zip(&output_weights[HIDDEN_SIZE..]) {
-        let them_clamped = them.clamp(0, QA);
-        output += i32::from(them_clamped * weight) * i32::from(them_clamped);
+            let them: *const i16 = them.0.as_ptr().add(i).cast();
+            let them_clamped = (*them).clamp(0, QA);
+            let them_weight: *const i16 = them_output_weights.as_ptr().add(i).cast();
+
+            output += i32::from(us_clamped * *us_weight) * i32::from(us_clamped);
+            output += i32::from(them_clamped * *them_weight) * i32::from(them_clamped);
+        }
     }
 
     output

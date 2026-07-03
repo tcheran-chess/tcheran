96797c7fe8e756e57e53216cf4b13be3ed9651bb
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-07-03 12:25:27 +0100

    Add a log::trace method for helping with debugging

--- engine/src/engine/util/log.rs
+++ engine/src/engine/util/log.rs
@@ -17,3 +17,21 @@ pub fn crashlog<S: AsRef<str>>(s: S) {
     writeln!(f, "[{}] {}", std::process::id(), s.as_ref()).unwrap();
     f.flush().unwrap();
 }
+
+#[allow(unused, reason = "Used for debugging")]
+pub fn trace<S: AsRef<str>>(s: S) {
+    let extension = format!("trace.{}.log", std::process::id());
+
+    let current_exe = std::env::current_exe().expect("Unable to determine current executable");
+
+    let path = current_exe.with_extension(extension);
+
+    let mut f = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+
+    writeln!(f, "{}", s.as_ref()).unwrap();
+    f.flush().unwrap();
+}

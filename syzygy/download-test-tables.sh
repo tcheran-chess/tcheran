4ca34d45bbedc77ddea39c848410689a6b7871bf
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-07-05 12:42:43 +0100

    Don't download atomic or antichess tables

--- syzygy/download-test-tables.sh
+++ syzygy/download-test-tables.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -e
 
 (cd tables/chess && wget -w 1 -nc -i TEST-SOURCE.txt)
-(cd tables/atomic && wget -w 1 -nc -i TEST-SOURCE.txt)
-(cd tables/antichess && wget -w 1 -nc -i TEST-SOURCE.txt)

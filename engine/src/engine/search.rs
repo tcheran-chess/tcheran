889ee27cef304225717f29bde5b89f590db5788e
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-07-27 15:51:07 +0100

    Move types from search.rs to types.rs

--- engine/src/engine/search.rs
+++ engine/src/engine/search.rs
@@ -8,30 +8,25 @@ pub fn init() {
     tables::init();
 }
 
-use std::{
-    sync::atomic::{AtomicU64, Ordering},
-    time::{Duration, Instant},
-};
+use std::time::Instant;
 
-use types::ScoreWindow;
+pub use types::*;
 
 use crate::{
-    chess::{Game, Move, Piece, Player, moves::MoveList},
+    chess::{Game, Move, moves::MoveList},
     engine::{
-        eval::{Eval, eval, nnue::NetworkStack},
+        eval::{Eval, eval},
         options::EngineOptions,
         params::*,
         search::{
             move_picker::{GenStage, MovePicker},
             principal_variation::PrincipalVariation,
-            tables::{Tables, lmr_reduction},
-            time_control::{StopControl, TimeStrategy},
-            types::{Depth, SearchResults},
+            tables::lmr_reduction,
+            time_control::StopControl,
         },
         see::see,
         tablebases::{Tablebase, Wdl},
-        transposition_table::{NodeBound, TranspositionTable},
-        util::buffered_atomic_counter::BufferedAtomicU64,
+        transposition_table::NodeBound,
     },
 };
 
@@ -40,286 +35,6 @@ pub const MAX_SEARCH_DEPTH: u8 = u8::MAX - 1;
 // Size used for arrays that are indexed by ply. Add one extra above MAX_PLIES_SIZE for safety
 // when we perform operations on 'the next ply'.
 pub const MAX_PLIES_ARRAY_SIZE: usize = MAX_SEARCH_DEPTH as usize + 1;
-
-pub struct PersistentState {
-    pub tt: TranspositionTable,
-    pub tablebase: Tablebase,
-
-    pub node_counter: AtomicU64,
-    pub tbhits_counter: AtomicU64,
-}
-
-impl PersistentState {
-    pub fn new(tt_size_mb: usize) -> Self {
-        Self::with_tablebase(tt_size_mb, &Tablebase::new())
-    }
-
-    pub fn with_tablebase(tt_size_mb: usize, tb: &Tablebase) -> Self {
-        Self {
-            tt: TranspositionTable::new(tt_size_mb),
-            tablebase: tb.clone(),
-
-            node_counter: AtomicU64::default(),
-            tbhits_counter: AtomicU64::default(),
-        }
-    }
-
-    pub fn new_search(&self) {
-        self.tt.new_generation();
-        self.reset_counters();
-    }
-
-    pub fn reset(&mut self, options: &EngineOptions) {
-        self.tt.reset(options.threads);
-        self.reset_counters();
-    }
-
-    fn reset_counters(&self) {
-        self.node_counter.store(0, Ordering::Relaxed);
-        self.tbhits_counter.store(0, Ordering::Relaxed);
-    }
-}
-
-pub struct ThreadData {
-    pub id: usize,
-    pub tables: Tables,
-    pub nnue: NetworkStack,
-    pub stack: SearchStack,
-}
-
-impl ThreadData {
-    pub fn new(id: usize) -> Self {
-        Self {
-            id,
-            tables: Tables::new(),
-            nnue: NetworkStack::new(),
-            stack: SearchStack::new(),
-        }
-    }
-
-    pub fn new_search(&mut self, game: &Game) {
-        self.nnue.setup(&game.board);
-        self.stack = SearchStack::new();
-    }
-
-    pub fn reset(&mut self) {
-        self.tables = Tables::new();
-        self.nnue = NetworkStack::new();
-        self.stack = SearchStack::new();
-    }
-
-    pub fn mut_refs(&mut self) -> (&mut Tables, &mut SearchStack, &mut NetworkStack) {
-        (&mut self.tables, &mut self.stack, &mut self.nnue)
-    }
-}
-
-#[repr(align(64))]
-pub struct SearchContext<'s> {
-    pub tt: &'s TranspositionTable,
-    pub tablebase: &'s Tablebase,
-    pub options: &'s EngineOptions,
-
-    pub tables: &'s mut Tables,
-    pub nnue: &'s mut NetworkStack,
-    pub stack: &'s mut SearchStack,
-
-    time_control: TimeStrategy,
-
-    pub id: usize,
-    pub max_depth_reached: u8,
-    pub completed_depth: Depth,
-    pub root_depth: Depth,
-    pub was_hard_stopped: bool,
-    pub nodes: BufferedAtomicU64<'s>,
-    pub tbhits: BufferedAtomicU64<'s>,
-}
-
-impl<'s> SearchContext<'s> {
-    pub fn new(
-        id: usize,
-        game: &Game,
-        tt: &'s TranspositionTable,
-        tablebase: &'s Tablebase,
-        node_counter: &'s AtomicU64,
-        tbhits_counter: &'s AtomicU64,
-        tables: &'s mut Tables,
-        search_stack: &'s mut SearchStack,
-        nnue: &'s mut NetworkStack,
-        time_control: TimeControl,
-        stop_control: StopControl,
-        options: &'s EngineOptions,
-    ) -> Self {
-        Self {
-            tt,
-            tablebase,
-            options,
-
-            tables,
-            stack: search_stack,
-            nnue,
-            time_control: TimeStrategy::new(game, time_control, stop_control, options),
-
-            id,
-            max_depth_reached: 0,
-            completed_depth: Depth::ZERO,
-            root_depth: Depth::ZERO,
-            was_hard_stopped: false,
-            nodes: BufferedAtomicU64::new(node_counter),
-            tbhits: BufferedAtomicU64::new(tbhits_counter),
-        }
-    }
-}
-
-impl SearchContext<'_> {
-    pub fn should_start_new_search(&mut self, depth: Depth) -> bool {
-        self.time_control
-            .should_start_new_search(self.nodes.get(), depth)
-    }
-
-    pub fn stopped(&mut self) -> bool {
-        self.time_control.stopped(self.nodes.get(), self.root_depth)
-    }
-
-    pub fn update_nodes_used(&mut self, mv: Move, nodes: u64) {
-        self.time_control.update_nodes_used(mv, nodes);
-    }
-
-    pub fn update_after_search(&mut self, best_move: Move, depth: Depth) {
-        self.completed_depth = depth;
-        self.time_control
-            .update_after_search(best_move, depth, self.nodes.get());
-    }
-}
-
-#[repr(align(64))]
-pub struct SearchStack([SearchStackEntry; MAX_PLIES_ARRAY_SIZE]);
-
-impl SearchStack {
-    pub const fn new() -> Self {
-        Self([const { SearchStackEntry::new() }; MAX_PLIES_ARRAY_SIZE])
-    }
-
-    pub fn get(&mut self, plies: u8) -> &mut SearchStackEntry {
-        &mut self.0[plies as usize]
-    }
-
-    pub fn last(&self, plies: u8) -> Option<&SearchStackEntry> {
-        self.get_prev(plies, 1)
-    }
-
-    pub fn get_prev(&self, plies: u8, i: usize) -> Option<&SearchStackEntry> {
-        let plies = plies as usize;
-
-        if i > plies {
-            return None;
-        }
-
-        Some(&self.0[plies - i])
-    }
-}
-
-pub struct SearchStackEntry {
-    pub mv: Option<(Move, Piece)>,
-    pub eval: Eval,
-
-    pub excluded_mv: Option<Move>,
-    pub double_extensions: u8,
-    pub fail_highs: u8,
-}
-
-impl SearchStackEntry {
-    pub const fn new() -> Self {
-        Self {
-            mv: None,
-            eval: Eval::MIN,
-
-            excluded_mv: None,
-            double_extensions: 0,
-            fail_highs: 0,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum TimeControl {
-    Clocks {
-        clocks: Clocks,
-        start_time: Instant,
-    },
-    ExactTime {
-        time: Duration,
-        start_time: Instant,
-    },
-    Depth(Depth),
-    Nodes {
-        soft: Option<u64>,
-        hard: Option<u64>,
-    },
-    Infinite,
-}
-
-#[derive(Debug, Clone)]
-pub struct Clocks {
-    pub clocks: [Option<Duration>; Player::N],
-    pub increments: [Option<Duration>; Player::N],
-    pub moves_to_go: Option<u32>,
-}
-
-impl Clocks {
-    pub fn for_player(&self, player: Player) -> (Duration, Duration) {
-        (self.clocks[player].unwrap_or_default(), self.increments[player].unwrap_or_default())
-    }
-}
-
-#[derive(Clone)]
-pub struct SearchResult {
-    pub id: usize,
-    pub mv: Move,
-    pub score: Eval,
-    pub pv: PrincipalVariation,
-    pub stats: SearchStats,
-}
-
-#[derive(Clone)]
-pub struct SearchStats {
-    pub time: Duration,
-    pub depth: u8,
-    pub seldepth: u8,
-    pub nodes: u64,
-    pub tbhits: u64,
-    pub hashfull: u64,
-}
-
-impl SearchStats {
-    pub fn from_ctx(ctx: &SearchContext<'_>) -> Self {
-        Self {
-            time: ctx.time_control.elapsed(),
-            depth: ctx.completed_depth.as_u8(),
-            seldepth: ctx.max_depth_reached,
-            nodes: ctx.nodes.get_global(),
-            tbhits: ctx.tbhits.get_global(),
-            hashfull: ctx.tt.occupancy(),
-        }
-    }
-}
-
-pub trait Reporter {
-    fn generic_report(&self, s: &str);
-
-    fn report_search_progress(&self, game: &Game, result: &SearchResult);
-
-    fn best_move(&self, game: &Game, mv: Move);
-}
-
-pub struct NullReporter;
-
-impl Reporter for NullReporter {
-    fn generic_report(&self, _: &str) {}
-
-    fn report_search_progress(&self, _: &Game, _: &SearchResult) {}
-
-    fn best_move(&self, _: &Game, _: Move) {}
-}
 
 pub fn search(
     game: &Game,
--- engine/src/engine/search/types.rs
+++ engine/src/engine/search/types.rs
@@ -1,6 +1,307 @@
-use std::{cmp::Ordering, sync::OnceLock};
+use std::{
+    cmp::Ordering,
+    sync::{OnceLock, atomic::AtomicU64},
+    time::{Duration, Instant},
+};
 
-use crate::engine::{eval::Eval, search::SearchResult};
+use crate::{
+    chess::{Game, Move, Piece, Player},
+    engine::{
+        eval::{Eval, nnue::NetworkStack},
+        options::EngineOptions,
+        search::{
+            MAX_PLIES_ARRAY_SIZE,
+            principal_variation::PrincipalVariation,
+            tables::Tables,
+            time_control::{StopControl, TimeStrategy},
+        },
+        tablebases::Tablebase,
+        transposition_table::TranspositionTable,
+        util::buffered_atomic_counter::BufferedAtomicU64,
+    },
+};
+
+pub struct PersistentState {
+    pub tt: TranspositionTable,
+    pub tablebase: Tablebase,
+
+    pub node_counter: AtomicU64,
+    pub tbhits_counter: AtomicU64,
+}
+
+impl PersistentState {
+    pub fn new(tt_size_mb: usize) -> Self {
+        Self::with_tablebase(tt_size_mb, &Tablebase::new())
+    }
+
+    pub fn with_tablebase(tt_size_mb: usize, tb: &Tablebase) -> Self {
+        Self {
+            tt: TranspositionTable::new(tt_size_mb),
+            tablebase: tb.clone(),
+
+            node_counter: AtomicU64::default(),
+            tbhits_counter: AtomicU64::default(),
+        }
+    }
+
+    pub fn new_search(&self) {
+        self.tt.new_generation();
+        self.reset_counters();
+    }
+
+    pub fn reset(&mut self, options: &EngineOptions) {
+        self.tt.reset(options.threads);
+        self.reset_counters();
+    }
+
+    fn reset_counters(&self) {
+        self.node_counter
+            .store(0, std::sync::atomic::Ordering::Relaxed);
+        self.tbhits_counter
+            .store(0, std::sync::atomic::Ordering::Relaxed);
+    }
+}
+
+pub struct ThreadData {
+    pub id: usize,
+    pub tables: Tables,
+    pub nnue: NetworkStack,
+    pub stack: SearchStack,
+}
+
+impl ThreadData {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            tables: Tables::new(),
+            nnue: NetworkStack::new(),
+            stack: SearchStack::new(),
+        }
+    }
+
+    pub fn new_search(&mut self, game: &Game) {
+        self.nnue.setup(&game.board);
+        self.stack = SearchStack::new();
+    }
+
+    pub fn reset(&mut self) {
+        self.tables = Tables::new();
+        self.nnue = NetworkStack::new();
+        self.stack = SearchStack::new();
+    }
+
+    pub fn mut_refs(&mut self) -> (&mut Tables, &mut SearchStack, &mut NetworkStack) {
+        (&mut self.tables, &mut self.stack, &mut self.nnue)
+    }
+}
+
+#[repr(align(64))]
+pub struct SearchContext<'s> {
+    pub tt: &'s TranspositionTable,
+    pub tablebase: &'s Tablebase,
+    pub options: &'s EngineOptions,
+
+    pub tables: &'s mut Tables,
+    pub nnue: &'s mut NetworkStack,
+    pub stack: &'s mut SearchStack,
+
+    time_control: TimeStrategy,
+
+    pub id: usize,
+    pub max_depth_reached: u8,
+    pub completed_depth: Depth,
+    pub root_depth: Depth,
+    pub was_hard_stopped: bool,
+    pub nodes: BufferedAtomicU64<'s>,
+    pub tbhits: BufferedAtomicU64<'s>,
+}
+
+impl<'s> SearchContext<'s> {
+    pub fn new(
+        id: usize,
+        game: &Game,
+        tt: &'s TranspositionTable,
+        tablebase: &'s Tablebase,
+        node_counter: &'s AtomicU64,
+        tbhits_counter: &'s AtomicU64,
+        tables: &'s mut Tables,
+        search_stack: &'s mut SearchStack,
+        nnue: &'s mut NetworkStack,
+        time_control: TimeControl,
+        stop_control: StopControl,
+        options: &'s EngineOptions,
+    ) -> Self {
+        Self {
+            tt,
+            tablebase,
+            options,
+
+            tables,
+            stack: search_stack,
+            nnue,
+            time_control: TimeStrategy::new(game, time_control, stop_control, options),
+
+            id,
+            max_depth_reached: 0,
+            completed_depth: Depth::ZERO,
+            root_depth: Depth::ZERO,
+            was_hard_stopped: false,
+            nodes: BufferedAtomicU64::new(node_counter),
+            tbhits: BufferedAtomicU64::new(tbhits_counter),
+        }
+    }
+}
+
+impl SearchContext<'_> {
+    pub fn should_start_new_search(&mut self, depth: Depth) -> bool {
+        self.time_control
+            .should_start_new_search(self.nodes.get(), depth)
+    }
+
+    pub fn stopped(&mut self) -> bool {
+        self.time_control.stopped(self.nodes.get(), self.root_depth)
+    }
+
+    pub fn update_nodes_used(&mut self, mv: Move, nodes: u64) {
+        self.time_control.update_nodes_used(mv, nodes);
+    }
+
+    pub fn update_after_search(&mut self, best_move: Move, depth: Depth) {
+        self.completed_depth = depth;
+        self.time_control
+            .update_after_search(best_move, depth, self.nodes.get());
+    }
+}
+
+#[repr(align(64))]
+pub struct SearchStack([SearchStackEntry; MAX_PLIES_ARRAY_SIZE]);
+
+impl SearchStack {
+    pub const fn new() -> Self {
+        Self([const { SearchStackEntry::new() }; MAX_PLIES_ARRAY_SIZE])
+    }
+
+    pub fn get(&mut self, plies: u8) -> &mut SearchStackEntry {
+        &mut self.0[plies as usize]
+    }
+
+    pub fn last(&self, plies: u8) -> Option<&SearchStackEntry> {
+        self.get_prev(plies, 1)
+    }
+
+    pub fn get_prev(&self, plies: u8, i: usize) -> Option<&SearchStackEntry> {
+        let plies = plies as usize;
+
+        if i > plies {
+            return None;
+        }
+
+        Some(&self.0[plies - i])
+    }
+}
+
+pub struct SearchStackEntry {
+    pub mv: Option<(Move, Piece)>,
+    pub eval: Eval,
+
+    pub excluded_mv: Option<Move>,
+    pub double_extensions: u8,
+    pub fail_highs: u8,
+}
+
+impl SearchStackEntry {
+    pub const fn new() -> Self {
+        Self {
+            mv: None,
+            eval: Eval::MIN,
+
+            excluded_mv: None,
+            double_extensions: 0,
+            fail_highs: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TimeControl {
+    Clocks {
+        clocks: Clocks,
+        start_time: Instant,
+    },
+    ExactTime {
+        time: Duration,
+        start_time: Instant,
+    },
+    Depth(Depth),
+    Nodes {
+        soft: Option<u64>,
+        hard: Option<u64>,
+    },
+    Infinite,
+}
+
+#[derive(Debug, Clone)]
+pub struct Clocks {
+    pub clocks: [Option<Duration>; Player::N],
+    pub increments: [Option<Duration>; Player::N],
+    pub moves_to_go: Option<u32>,
+}
+
+impl Clocks {
+    pub fn for_player(&self, player: Player) -> (Duration, Duration) {
+        (self.clocks[player].unwrap_or_default(), self.increments[player].unwrap_or_default())
+    }
+}
+
+#[derive(Clone)]
+pub struct SearchResult {
+    pub id: usize,
+    pub mv: Move,
+    pub score: Eval,
+    pub pv: PrincipalVariation,
+    pub stats: SearchStats,
+}
+
+#[derive(Clone)]
+pub struct SearchStats {
+    pub time: Duration,
+    pub depth: u8,
+    pub seldepth: u8,
+    pub nodes: u64,
+    pub tbhits: u64,
+    pub hashfull: u64,
+}
+
+impl SearchStats {
+    pub fn from_ctx(ctx: &SearchContext<'_>) -> Self {
+        Self {
+            time: ctx.time_control.elapsed(),
+            depth: ctx.completed_depth.as_u8(),
+            seldepth: ctx.max_depth_reached,
+            nodes: ctx.nodes.get_global(),
+            tbhits: ctx.tbhits.get_global(),
+            hashfull: ctx.tt.occupancy(),
+        }
+    }
+}
+
+pub trait Reporter {
+    fn generic_report(&self, s: &str);
+
+    fn report_search_progress(&self, game: &Game, result: &SearchResult);
+
+    fn best_move(&self, game: &Game, mv: Move);
+}
+
+pub struct NullReporter;
+
+impl Reporter for NullReporter {
+    fn generic_report(&self, _: &str) {}
+
+    fn report_search_progress(&self, _: &Game, _: &SearchResult) {}
+
+    fn best_move(&self, _: &Game, _: Move) {}
+}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct ScoreWindow {

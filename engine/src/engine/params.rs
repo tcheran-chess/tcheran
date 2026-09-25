e8a369d35b6cea9c345fa985944a21a50295f577
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-09-25 11:04:36 +0100

    make node tm more aggressive

--- engine/src/engine/params.rs
+++ engine/src/engine/params.rs
@@ -124,7 +124,7 @@ parameters!(
 
     node_tm_base: u32 = 2630 [10..5120];
     node_tm_multiplier: u32 = 1700 [10..5120];
-    node_tm_min: u32 = 900 [10..1000];
+    node_tm_min: u32 = 500 [10..1000];
 );
 
 #[cfg(test)]

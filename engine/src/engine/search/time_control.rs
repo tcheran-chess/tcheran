04fc8798a381287791f7230caf5e9aaa2742d32d
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-09-25 11:09:04 +0100

    use params for best move stability

--- engine/src/engine/search/time_control.rs
+++ engine/src/engine/search/time_control.rs
@@ -39,6 +39,10 @@ struct Params {
     soft_time_multiplier: f32,
     hard_time_multiplier: f32,
 
+    best_move_tm_base: f32,
+    best_move_tm_multiplier: f32,
+    best_move_tm_min: f32,
+
     node_tm_base: f32,
     node_tm_multiplier: f32,
     node_tm_min: f32,
@@ -54,6 +58,10 @@ impl Params {
             increment_to_use: scale_param(increment_to_use()),
             soft_time_multiplier: scale_param(soft_time_multiplier()),
             hard_time_multiplier: scale_param(hard_time_multiplier()),
+
+            best_move_tm_base: scale_param(best_move_tm_base()),
+            best_move_tm_multiplier: scale_param(best_move_tm_multiplier()),
+            best_move_tm_min: scale_param(best_move_tm_min()),
 
             node_tm_base: scale_param(node_tm_base()),
             node_tm_multiplier: scale_param(node_tm_multiplier()),
@@ -111,7 +119,6 @@ impl StopControl {
 }
 
 const CHECK_TERMINATION_NODE_FREQUENCY: u64 = 2048;
-const BEST_MOVE_STABILITY_TIME_MULTIPLIERS: [f32; 5] = [2.50, 1.20, 1.00, 0.80, 0.75];
 
 impl TimeStrategy {
     pub fn new(
@@ -261,11 +268,9 @@ impl TimeStrategy {
         let mut scale = 1.0;
 
         let best_move_scale_adjustment = {
-            if depth >= best_move_stability_initial_depth() {
-                BEST_MOVE_STABILITY_TIME_MULTIPLIERS[self.best_move_stability.min(4)]
-            } else {
-                1.0
-            }
+            (self.params.best_move_tm_base
+                - self.best_move_stability as f32 * self.params.best_move_tm_multiplier)
+                .max(self.params.best_move_tm_min)
         };
 
         let node_scale_adjustment = {
